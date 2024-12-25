1491eec366181d10772653ac7adbc7ae5750a044
Joositoo <161719500+Joositoo@users.noreply.github.com>
2024-12-25 02:33:01 +0100

    Add files via upload

--- Biblioteca/src/main/java/Biblioteca/Controlador/ControlPrestamo.java
+++ Biblioteca/src/main/java/Biblioteca/Controlador/ControlPrestamo.java
@@ -7,6 +7,7 @@ import Biblioteca.Modelo.Usuario;
 import Biblioteca.Vista.Consola;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class ControlPrestamo {
@@ -19,6 +20,20 @@ public class ControlPrestamo {
                 break;
             case 2:
                 modificaPrestamo();
+                break;
+            case 3:
+                eliminaPrestamo();
+                break;
+            case 4:
+                verPrestamos();
+                break;
+            case 5:
+                seleccionaPrestamo();
+                break;
+            case 6:
+                Consola.menuAdmin();
+                break;
+            case 7:
                 break;
         }
     }
@@ -58,5 +73,95 @@ public class ControlPrestamo {
         Consola.menuPrestamos();
     }
 
-    public static void modificaPrestamo() {}
+    public static void modificaPrestamo() {
+        int id;
+        int idEjemplar;
+
+        DAO<Prestamo, Integer> daoPrestamo = new DAO<>(Prestamo.class, Integer.class);
+        DAO<Ejemplar, Integer> daoEjemplar = new DAO<>(Ejemplar.class, Integer.class);
+
+        System.out.println("*****************************************");
+        System.out.println("*                                       *");
+        System.out.println("*        MODIFICAR PRÉSTAMO             *");
+        System.out.println("*                                       *");
+        System.out.println("*****************************************");
+        System.out.println("                                         ");
+        System.out.println("Introduce el id del préstamo a modificar: ");
+        id = scann.nextInt();
+
+        System.out.println("Introduce el id del ejemplar nuevo: ");
+        idEjemplar = scann.nextInt();
+
+        Ejemplar ejemplar = daoEjemplar.findById(idEjemplar);
+
+        Prestamo prestamo = daoPrestamo.findById(id);
+
+        prestamo.setEjemplar(ejemplar);
+        prestamo.setFechaInicio(LocalDate.now());
+        prestamo.setFechaDevolucion(LocalDate.now().plusDays(15));
+
+        daoPrestamo.update(prestamo);
+
+        Consola.menuPrestamos();
+    }
+
+    public static void eliminaPrestamo() {
+        int id;
+        DAO<Prestamo, Integer> daoPrestamo = new DAO<>(Prestamo.class, Integer.class);
+
+        System.out.println("*****************************************");
+        System.out.println("*                                       *");
+        System.out.println("*         ELIMINAR PRÉSTAMO             *");
+        System.out.println("*                                       *");
+        System.out.println("*****************************************");
+        System.out.println("                                         ");
+        System.out.print("Indica el id del préstamo a eliminar: ");
+        id = scann.nextInt();
+
+        Prestamo prestamo = daoPrestamo.findById(id);
+
+        daoPrestamo.delete(prestamo);
+
+        Consola.menuPrestamos();
+    }
+
+    public static void verPrestamos() {
+        DAO<Prestamo, Integer> daoPrestamo = new DAO<>(Prestamo.class, Integer.class);
+        List<Prestamo> listaPrestamos;
+
+        listaPrestamos = daoPrestamo.findAll();
+
+        System.out.println("*****************************************");
+        System.out.println("*                                       *");
+        System.out.println("*         LISTA DE PRÉSTAMOS            *");
+        System.out.println("*                                       *");
+        System.out.println("*****************************************");
+        System.out.println("                                         ");
+
+        for (Prestamo p : listaPrestamos) {
+            System.out.println(p);
+        }
+
+        Consola.menuPrestamos();
+    }
+
+    public static void seleccionaPrestamo(){
+        int id;
+        DAO<Prestamo, Integer> daoPrestamo = new DAO<>(Prestamo.class, Integer.class);
+
+        System.out.println("******************************************");
+        System.out.println("*                                        *");
+        System.out.println("*         SELECCIONA PRÉSTAMO            *");
+        System.out.println("*                                        *");
+        System.out.println("******************************************");
+        System.out.println("                                          ");
+        System.out.print("Indica el id del préstamo: ");
+        id = scann.nextInt();
+
+        Prestamo prestamo = daoPrestamo.findById(id);
+
+        System.out.println(prestamo);
+
+        Consola.menuPrestamos();
+    }
 }
